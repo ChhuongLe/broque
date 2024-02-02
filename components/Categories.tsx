e5153967d45ef2963bd6e2d7f2ebc5f724cf4e0c
Chhuong Le <chhuongle@gmail.com>
2024-02-02 14:12:42 -0800

    add changes to helper fetchURL and update jacket handleClick function

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -2,11 +2,13 @@
 
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
-import { fetchAllProducts } from '@/helpers/helper';
+import { fetchAllProducts, fetchThumbnails } from '@/helpers/helper';
+import { set } from 'firebase/database';
 
 export default function Categories () {
   const [data, setData] = useState<any[]>([]);
-
+  const [promise4All, setPromise4all] = useState<any[]>([])
+  let arr: any[] = [];
   useEffect(()=>{
     fetchAllProducts()
       .then(data => {
@@ -18,13 +20,26 @@ export default function Categories () {
   },[]);
 
   // data should not change
-  const jacketId = data[0], shoesId = data[1], accessoriesId = data[2], shirtsId = data[3];
+  const jacketId = data[0], shoesId = data[1], accessoriesId = data[2], shirtsId = data[3] , pantsId = data[4];
+
+  const handleClick = (category: string) => {
+    if(category === "Jackets") jacketId.forEach((element: number) => {
+      arr.push(fetchThumbnails(element))
+      Promise.all(arr)
+      .then((value)=>console.log(value))
+    });
+
+    if(category === "Accessories") accessoriesId.forEach((element: number) => arr.push(fetchThumbnails(element)));
+    if(category === "Pants") pantsId.forEach((element: number) => arr.push(fetchThumbnails(element)));
+    if(category === "Shirts") shirtsId.forEach((element: number) => arr.push(fetchThumbnails(element)));
+    if(category === "Shoes") shoesId.forEach((element: number) => arr.push(fetchThumbnails(element)));
+  }
 
   return (
     <div>
       <h1 className='text-white text-3xl font-bold pt-10 pb-5 w-screen pl-5'>Shop Categories</h1>
       <div className='flex flex-row space-x-5 pb-10 items-center justify-around'>
-        <div className='flex relative w-[200px] xl:w-[350px] h-[200px] xl:h-[350px] hover:cursor-pointer hover:grayscale transition duration-300 ease-in-out z-0'>
+        <div className='flex relative w-[200px] xl:w-[350px] h-[200px] xl:h-[350px] hover:cursor-pointer hover:grayscale transition duration-300 ease-in-out z-0' onClick={e=>(handleClick(e.target.innerText))}>
           <div className='flex absolute h-full w-full opacity-0 hover:opacity-70 transition duration-300 ease-in-out z-10 items-center justify-center hover:bg-black'>
             <p className='text-sm lg:text-2xl font-bold text-white opacity-100'>Jackets</p>
           </div>
@@ -35,25 +50,25 @@ export default function Categories () {
             objectFit='cover'
           />
         </div>
-        <div className='flex relative w-[200px] xl:w-[350px] h-[200px] xl:h-[350px] hover:cursor-pointer hover:grayscale transition duration-300 ease-in-out z-0'>
+        <div className='flex relative w-[200px] xl:w-[350px] h-[200px] xl:h-[350px] hover:cursor-pointer hover:grayscale transition duration-300 ease-in-out z-0' onClick={e=>(handleClick(e.target.innerText))}>
           <div className='flex absolute h-full w-full opacity-0 hover:opacity-70 transition duration-300 ease-in-out z-10 items-center justify-center hover:bg-black'>
             <p className='text-sm lg:text-2xl font-bold text-white opacity-100'>Accessories</p>
           </div>
           <Image src="https://images.unsplash.com/photo-1553830591-d8632a99e6ff?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80" alt="" layout='fill' objectFit='cover'/>
         </div>
-        <div className='flex relative w-[200px] xl:w-[350px] h-[200px] xl:h-[350px] hover:cursor-pointer hover:grayscale transition duration-300 ease-in-out z-0'>
+        <div className='flex relative w-[200px] xl:w-[350px] h-[200px] xl:h-[350px] hover:cursor-pointer hover:grayscale transition duration-300 ease-in-out z-0' onClick={e=>(handleClick(e.target.innerText))}>
           <div className='flex absolute h-full w-full opacity-0 hover:opacity-70 transition duration-300 ease-in-out z-10 items-center justify-center hover:bg-black'>
             <p className='text-sm lg:text-2xl font-bold text-white opacity-100'>Pants</p>
           </div>
           <Image src="https://images.unsplash.com/photo-1552902865-b72c031ac5ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80" alt="" layout='fill' objectFit='cover'/>
         </div>
-        <div className='flex relative w-[200px] xl:w-[350px] h-[200px] xl:h-[350px] hover:cursor-pointer hover:grayscale transition duration-300 ease-in-out z-0'>
+        <div className='flex relative w-[200px] xl:w-[350px] h-[200px] xl:h-[350px] hover:cursor-pointer hover:grayscale transition duration-300 ease-in-out z-0' onClick={e=>(handleClick(e.target.innerText))}>
           <div className='flex absolute h-full w-full opacity-0 hover:opacity-70 transition duration-300 ease-in-out z-10 items-center justify-center hover:bg-black'>
             <p className='text-sm lg:text-2xl font-bold text-white opacity-100'>Shoes</p>
           </div>
           <Image src="https://images.unsplash.com/photo-1544441892-794166f1e3be?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80" alt="" layout='fill' objectFit='cover'/>
         </div>
-        <div className='flex relative w-[200px] xl:w-[350px] h-[200px] xl:h-[350px] hover:cursor-pointer hover:grayscale transition duration-300 ease-in-out z-0'>
+        <div className='flex relative w-[200px] xl:w-[350px] h-[200px] xl:h-[350px] hover:cursor-pointer hover:grayscale transition duration-300 ease-in-out z-0' onClick={e=>(handleClick(e.target.innerText))}>
           <div className='flex absolute h-full w-full opacity-0 hover:opacity-70 transition duration-300 ease-in-out z-10 items-center justify-center hover:bg-black'>
             <p className='text-sm lg:text-2xl font-bold text-white opacity-100'>Shirts</p>
           </div>
--- helpers/helper.ts
+++ helpers/helper.ts
@@ -4,19 +4,22 @@ export const fetchAllProducts = async () => {
   const res = await fetch("http://3.137.191.193/products/?count=500")
   let products = await res.json();
   // sort here instead of in the Categories component???
-  let jacketsId = [], shoesId = [], accessoriesId = [], shirtsId = [], allIds = [];
+  let jacketsId = [], shoesId = [], accessoriesId = [], shirtsId = [], pantsId = [], allIds = [];
 
   for(let obj of products) {
     if(obj.category === "Jacket") jacketsId.push(obj.id)
     if(obj.category === "Shoes" || obj.category === "Kicks" || obj.category === "Dress Shoes" || obj.category === "Heels") shoesId.push(obj.id)
     if(obj.category === "Shirt") shirtsId.push(obj.id)
     if(obj.category === "Accessories" || obj.category === "Sunglasses" || obj.category === "Cap") accessoriesId.push(obj.id)
+    if(obj.category === "Pants") pantsId.push(obj.id)
   }
-  allIds = [jacketsId, shoesId, shirtsId, accessoriesId]
+  allIds = [jacketsId, shoesId, shirtsId, accessoriesId, pantsId]
   return allIds;
 }
 
 export const fetchThumbnails = async (id: number) => {
   const res = await(fetch(`http://3.137.191.193/products/${id}/styles`))
   let styles = await res.json();
+  let thumbnail_url = await styles.results[0].photos[0].thumbnail_url;
+  return thumbnail_url;
 }
