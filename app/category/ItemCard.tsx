f2f94c77571120b9beda64c1465629d4d2c41441
Chhuong Le <chhuongle@gmail.com>
2024-02-13 14:55:36 -0800

    add styling to category page

--- app/category/ItemCard.tsx
+++ app/category/ItemCard.tsx
@@ -3,11 +3,12 @@ import Image from "next/image";
 
 export default function ItemCard({id, price, name, description, img}) {
   return (
-    <div>
-      <Image src={img} alt="" width={60} height={60}/>
-      {price}
-      {name}
-      <span>{description}</span>
+    <div className="flex flex-col w-[300px] h-[300px]">
+      <div className="flex relative w-[200px] h-[200px]">
+        <Image src={img} alt="" objectFit="cover" layout="fill"/><br />
+      </div>
+      <p className="ml-[-225px]">Price: {price}</p>
+      <p className="mr-[300px]">{name}</p>
     </div>
   )
 }
--- app/category/page.tsx
+++ app/category/page.tsx
@@ -9,24 +9,26 @@ export default function Category () {
     state.itemMap,
   ])
 
-  console.log(itemMap)
-
   let motto = ""
 
   if(category === "Jackets") {
     motto = "It's cold out there! Take these with you!"
+  } else if (category === "Accessories") {
+    motto = "Look cool you fool! Take these accessories out there and rock them socks!"
   }
 
   return (
-    <div>
-      <h2>{category}</h2>
-      <span>{motto}</span>
-      <div>
-      {itemMap!.map((el)=>{
-        return (
-          <ItemCard id={el.id} price={el.price} name={el.name} description={el.description} img={el.img} />
-        )
-      })}
+    <div className='flex flex-col py-10 w-screen'>
+      <div className='items-center text-center mx-auto max-w-7xl'>
+        <h2 className='text-3xl font-bold p-3'>{category}</h2>
+        <span className='text-m'>{motto}</span>
+        <div className='grid grid-cols-2 py-10 mr-[-100px] gap-x-32'>
+          {itemMap!.map((el)=>{
+            return (
+              <ItemCard id={el.id} price={el.price} name={el.name} description={el.description} img={el.img} />
+            )
+          })}
+        </div>
       </div>
     </div>
   )
