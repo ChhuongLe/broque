a13c864c88932a6cbaae0f8f108821fb53f0079d
Chhuong Le <chhuongle@gmail.com>
2024-02-15 14:53:17 -0800

    fix formatting of item card

--- app/category/ItemCard.tsx
+++ app/category/ItemCard.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 
 export default function ItemCard({id, price, name, description, img}) {
   return (
-    <div className="flex flex-col w-[300px] h-[300px]">
-      <div className="flex relative w-[200px] h-[200px]">
+    <div className="flex flex-col w-[300px] h-[300px] lg:w-[600px] lg:h-[600px]">
+      <div className="flex relative w-[200px] h-[200px] lg:w-[500px] lg:h-[500px]">
         <Image src={img} alt="" objectFit="cover" layout="fill"/><br />
       </div>
-      <p className="ml-[-225px]">Price: {price}</p>
-      <p className="mr-[300px]">{name}</p>
+      <p className="mr-auto text-md lg:text-xl ">Price: {price}</p>
+      <p className="mr-auto text-md lg:text-xl">{name}</p>
     </div>
   )
 }
