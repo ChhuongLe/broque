495c1fba7aa38fb956699eb96b17ea10faa4f103
Chhuong Le <chhuongle@gmail.com>
2024-02-28 04:49:33 -0800

    initalize the footer

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import { initFirebase } from "@/firebase/firebase";
 import Banner from "@/components/Banner";
 import Categories from "@/components/Categories";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main className="flex flex-col bg-[#387d7a] w-screen h-screen">
         <Banner />
         <Categories />
+        <Footer />
     </main>
   );
 }
---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div>
+      Hello from the foot
+    </div>
+  )
+}
